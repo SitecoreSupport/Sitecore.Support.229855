d1a21237412b4a87d9c2c99c2f626caec846cc13
DK\tpe <tpe@sitecore.net>
2019-01-30 14:30:50 +0200

    Fix issue in the CloudSearchDocumentBuilder class

---
+++ src/Sitecore.Support.229855/ContentSearch/Azure/CloudSearchDocumentBuilder.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.ContentSearch.Azure
+{
+  using Sitecore.ContentSearch;
+  using Sitecore.ContentSearch.ComputedFields;
+
+  public class CloudSearchDocumentBuilder : Sitecore.ContentSearch.Azure.CloudSearchDocumentBuilder
+  {
+
+    protected override void AddComputedIndexField(IComputedIndexField computedIndexField, object fieldValue)
+    {
+      this.AddField(computedIndexField.FieldName, fieldValue, false);
+    }
+
+    public CloudSearchDocumentBuilder(IIndexable indexable, IProviderUpdateContext context) : base(indexable, context)
+    {
+    }
+  }
+}
